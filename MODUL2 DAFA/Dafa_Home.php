1ab483d1abb7342f8b84612ca2a16c83ec80824e
dafandika <115630306+dafandika@users.noreply.github.com>
2022-10-30 21:44:50 +0700

    Add files via upload

---
+++ MODUL2 DAFA/Dafa_Home.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>TP Modul 2 - Dafa Andika - 1202204206</title>
+</head>
+<body style="background-color:whitesmoke;">
+    <nav class="navbar-light fixed-top" style="background-color: #607EAA;">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <h6 style="text-align: center;color:antiquewhite">
+                    <a class="nav-link active text-light" href="Dafa_Home.php">Home</a>
+                </h6>
+            </li>
+            <li class="nav-item">
+                <h6 style="text-align: center;color:antiquewhite">
+                    <a class="nav-link active text-light" href="Dafa_BookingPage.php">Booking</a>
+                </h6>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container" style="text-align: center; paddding-top: 0.5cm;">
+        <div class="class text-center mb-5 & text-primary">
+            <h3 style="text-align: center; color: black">
+                <b>
+                    WELCOME TO EAD RENT
+                </b>
+            </h3>
+            <p style="text-align: center; color:#607EAA">
+                <b>
+                    Find your best deal here!
+                </b>
+            </p>
+        </div>
+
+        <div class="container mt-5">
+            <div class="card-deck">
+                <div class="card text-center" style="width:15rem;">
+                    <img src="ioniq.jpg" class="card-img-top" style="height: 10rem;" alt="Hyundai Ioniq">
+
+                <div class="card-body" style="text-align: left;">
+                    <h5 class="card-title">Hyundai Ioniq</h5>
+                    <p>
+                        <b class="card-title">Rp. 800.000/Day</b>
+                    </p>
+                    <br>
+                    <div class="card-text" style="text-align: center;">
+                    <ul class="list-group list-group-flush">
+                        <div class="list-group-item" style="color: #1C3879;">
+                            <b>4 Seat</b>
+                        </div>
+                        <li class="list-group-item" style="color: #1C3879;">
+                            <b>2000 cc</b>
+                        </li>
+                        <li class="list-group-item" style="color: #1C3879;">
+                            <b>Electric/Automatic</b>
+                        </li>
+                        <li class="list-group-item"></li>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="BookingPage.php?type=Hyundai Ioniq" class="btn-primary form-control">
+                            <b>Book now</b>
+                        </a>
+                </div>
+                </ul>
+                </div>
+                <div class="card text-center" style="width:15rem;">
+                    <img src="jimny.jpg" class="card-img-top" style="height: 10rem;" alt="Suzuki Jimny">
+
+                <div class="card-body" style="text-align: left;">
+                    <h5 class="card-title">Suzuki Jimny</h5>
+                    <p>
+                        <b class="card-title">Rp. 1.200.000/Day</b>
+                    </p>
+                    <br>
+                    <div class="card-text" style="text-align: center;">
+                    <ul class="list-group list-group-flush">
+                        <div class="list-group-item" style="color: #1C3879;">
+                            <b>4 Seat</b>
+                        </div>
+                        <li class="list-group-item" style="color: #1C3879;">
+                            <b>2500 cc</b>
+                        </li>
+                        <li class="list-group-item" style="color: #1C3879;">
+                            <b>Manual</b>
+                        </li>
+                        <li class="list-group-item"></li>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="BookingPage.php?type=Hyundai Ioniq" class="btn-primary form-control">
+                            <b>Book now</b>
+                        </a>
+                </div>
+                </ul>
+                </div>
+
+                <div class="card text-center" style="width:15rem;">
+                    <img src="stargazer.jpg" class="card-img-top" style="height: 10rem;" alt="Hyundai Stargazer">
+
+                <div class="card-body" style="text-align: left;">
+                    <h5 class="card-title">Hyundai Stargazer</h5>
+                    <p>
+                        <b class="card-title">Rp. 1.000.000/Day</b>
+                    </p>
+                    <br>
+                    <div class="card-text" style="text-align: center;">
+                    <ul class="list-group list-group-flush">
+                        <div class="list-group-item" style="color: #1C3879;">
+                            <b>6 Seat</b>
+                        </div>
+                        <li class="list-group-item" style="color: #1C3879;">
+                            <b>2000 cc</b>
+                        </li>
+                        <li class="list-group-item" style="color: #1C3879;">
+                            <b>Manual</b>
+                        </li>
+                        <li class="list-group-item"></li>
+                        </div>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="BookingPage.php?type=Hyundai Ioniq" class="btn-primary form-control">
+                            <b>Book now</b>
+                        </a>
+                </div>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer style="text-align: center; color: black">
+        <b>(Dafa_1202204206)</b>
+    </footer>
+</body>
+</html>
